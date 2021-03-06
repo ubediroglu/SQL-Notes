790d556ce1cacb8dfd98e392d153a040f8f927bc
Umut Bediroğlu <78664003+UmutBediroglu@users.noreply.github.com>
2021-03-06 23:13:53 +0300

    Update 6_join.sql

--- codes/6_join.sql
+++ codes/6_join.sql
@@ -1,1 +1,15 @@
+/* type of join 
+1- INNER JOIN: only join intersections
+2- LEFT JOIN: left table and join intersections
+3- RIGHT JOIN: right table and join intersections
+4- FULL OUTER JOIN: all of them join */
 
+/* association */
+SELECT * FROM table_1
+JOIN table_2
+ON table_1.id = table_2.id;
+
+/* union is add two table but tables should same number of columns */
+SELECT * FROM table_1
+UNION
+SELECT * FROM table_2;
