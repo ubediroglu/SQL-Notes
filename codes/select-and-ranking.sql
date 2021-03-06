80e484404904ac64daebdfcb29b35363a3f6a930
Umut Bediroğlu <78664003+UmutBediroglu@users.noreply.github.com>
2021-03-06 17:13:56 +0300

    Update select-and-ranking.sql

--- codes/select-and-ranking.sql
+++ codes/select-and-ranking.sql
@@ -7,19 +7,30 @@ SELECT DISTINCT column_1 from table_1 ;
 
 /* data selection */
 SELECT * FROM table_1
-WHERE column > 4;
+WHERE column_1 > 4;
 
 /* NOTE: Selects "%a" the ones with a at the end, selects "a%" with a beginning. */
 SELECT * FROM table_1
-WHERE column like= "aa";
+WHERE column_1 like= "aa";
 
 SELECT*FROM table_1
 WHERE column BETWEEN "A" and "C";
 
 SELECT*FROM table_1
-WHERE column is NULL;
+WHERE column_1 is NULL;
 
-/* sort of data
+/* ranking of data */
+/* if you want use "asc" to A-Z and desc to Z-A */
+SELECT * FROM table_1
+WHERE column_1 > 4
+ORDER BY column_2;
 
+SELECT * FROM table_1
+WHERE column_1 > 4
+ORDER BY column_2 ASC;
+
+/* ranking limitins, sorts the lowest 3 numbers*/
+SELECT * FROM table_1
+ORDER BY column_2 ASC LIMIT 3;
 
 
