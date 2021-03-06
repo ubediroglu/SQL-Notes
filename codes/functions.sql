474b3d3a595e1068682040a29c3fd02b2f1a5f84
Umut Bediroğlu <78664003+UmutBediroglu@users.noreply.github.com>
2021-03-06 15:53:27 +0300

    Update functions.sql

--- codes/functions.sql
+++ codes/functions.sql
@@ -1,3 +1,19 @@
 /* number of pieces with "count()" */
 SELECT count(*) FROM table;
 
+/* addition with "sum()" */
+SELECT sum(column) FROM table;
+
+/* find maximum value with "max()" */
+SELECT max(column) FROM table;
+
+/* find minimum value with "max()" */
+SELECT min(column) FROM table;
+
+/* average of value with "avg()" */
+SELECT avg(column) FROM table;
+
+/* round the number with "round()" */
+SELECT round(column) FROM table;
+
+
